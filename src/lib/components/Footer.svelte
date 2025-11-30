0977a0083a0db74ba77b4f0c22bb2d9ece4caa2e
ion05 <49789627+ion05@users.noreply.github.com>
2025-11-30 15:27:29 -0500

    footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -10,6 +10,20 @@
 		<p class={footerStyles.clubName}>
 			<a href="https://humanoidrobot.club/" class={footerStyles.clubLink}>humanoid robot club</a>
 		</p>
+		<div class={footerStyles.socialMedia}>
+			<a href="https://www.instagram.com/humanoid.purdue/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" class={footerStyles.socialLink}>
+				<img src="/icon-instagram.svg" alt="Instagram" class={footerStyles.socialIcon} />
+			</a>
+			<a href="https://www.linkedin.com/company/humanoid-purdue/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" class={footerStyles.socialLink}>
+				<img src="/icon-linkedin.svg" alt="LinkedIn" class={footerStyles.socialIcon} />
+			</a>
+			<a href="https://x.com/HumanoidPurdue" target="_blank" rel="noopener noreferrer" aria-label="Twitter" class={footerStyles.socialLink}>
+				<img src="/icon-twitter.svg" alt="Twitter" class={footerStyles.socialIcon} />
+			</a>
+		</div>
+		<p class={footerStyles.copyright}>
+			© 2025 Humanoid Robot Club
+		</p>
 	</div>
 </footer>
 
