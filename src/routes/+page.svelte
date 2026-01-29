763e9d40b33988f9d9d2e3c6ce4973ab5e36e873
ion05 <49789627+ion05@users.noreply.github.com>
2026-01-29 16:08:37 -0500

    Update meta description: Guinness Record, machining tools, April 17-19 2026

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,11 +13,11 @@
 
 <svelte:head>
 	<title>StarkHacks</title>
-	<meta name="description" content="Join 1000+ students at the World's Largest hardware Hackathon to build hardware innovations. Free 3D Printers, sensors, motors, & tons of workshops. April 17-19 2026 at Purdue University." />
+	<meta name="description" content="Join 1000+ students and set an Official Guinness Record at the World's Largest hardware Hackathon. Free 3D Printers, machining tools, sensors, motors, & tons of workshops. April 17-19, 2026 at Purdue University." />
 	<meta property="og:title" content="StarkHacks" />
-	<meta property="og:description" content="Join 1000+ students at the World's Largest hardware Hackathon to build hardware innovations. Free 3D Printers, sensors, motors, & tons of workshops. April 17-19 2026 at Purdue University." />
+	<meta property="og:description" content="Join 1000+ students and set an Official Guinness Record at the World's Largest hardware Hackathon. Free 3D Printers, machining tools, sensors, motors, & tons of workshops. April 17-19, 2026 at Purdue University." />
 	<meta name="twitter:title" content="StarkHacks" />
-	<meta name="twitter:description" content="Join 1000+ students at the World's Largest hardware Hackathon to build hardware innovations. Free 3D Printers, sensors, motors, & tons of workshops. April 17-19 2026 at Purdue University." />
+	<meta name="twitter:description" content="Join 1000+ students and set an Official Guinness Record at the World's Largest hardware Hackathon. Free 3D Printers, machining tools, sensors, motors, & tons of workshops. April 17-19, 2026 at Purdue University." />
 </svelte:head>
 
 <Navigation />
