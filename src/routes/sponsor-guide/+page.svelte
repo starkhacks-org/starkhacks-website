cbe24977005e002a4598d959648424e3c6b001c0
ion05 <49789627+ion05@users.noreply.github.com>
2026-02-24 01:21:09 -0500

    sponsor guide

---
+++ src/routes/sponsor-guide/+page.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<meta name="robots" content="noindex, nofollow" />
+	<title>StarkHacks Sponsors Guide</title>
+</svelte:head>
+
+<div class="container">
+	<iframe
+		src="/sponsors-guide.pdf"
+		title="StarkHacks Sponsors Guide"
+		class="pdf-viewer"
+	></iframe>
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background: #000;
+	}
+
+	.container {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.pdf-viewer {
+		flex: 1;
+		width: 100%;
+		height: 100%;
+		border: none;
+	}
+</style>
